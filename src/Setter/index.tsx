19258a1bcb3f070969dbbf676ac606b05acdd12b
HuiAzir <huiazr@qq.com>
2020-11-20 23:13:29 +0800

    feat: init setter

--- src/Setter/index.tsx
+++ src/Setter/index.tsx
@@ -2,15 +2,9 @@ import React, { memo } from 'react';
 import classnames from 'classnames';
 import styles from './index.module.scss';
 
-const Setter = ({
-  className = '',
-}) => {
+const Setter = ({ className = '' }) => {
   const _className = classnames(className, styles.container);
-  return (
-    <div className={_className}>
-      Setter
-    </div>
-  );
+  return <div className={_className}>Setter123123</div>;
 };
 
 export default memo(Setter);
