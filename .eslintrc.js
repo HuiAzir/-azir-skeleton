b3c9599af3106ab84aaf902f86f85f541f772f28
HuiAzir <huiazr@qq.com>
2020-11-20 23:10:24 +0800

    chore:  优化工程

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -51,5 +56,6 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    '@typescript-eslint/indent': ['error', 2],
   },
 };
